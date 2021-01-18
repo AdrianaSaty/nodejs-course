d50c49d1bb768f5b9ac50977b36bab42683669a4
Adriana Saty <adri-saty@hotmail.com>
2021-01-18 16:05:05 -0300

    routing requests

--- section3-basics/app.js
+++ section3-basics/app.js
@@ -3,13 +3,17 @@ const http = require('http');
 
 
 const server = http.createServer(((req, res) => {
-    console.log('req',req.url)
-    console.log('method',req.method)
-    console.log('headers',req.headers)
-    // process.exit()
+    const url = req.url
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head>enter message</head>')
+        res.write('<body><form action="/message" method"POST"><input type="text"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
-    res.write('<head>hiii</head>')
+    res.write('<head>nao é a home</head>')
     res.write('</html>')
     res.end()
 }));
