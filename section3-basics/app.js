7fc4e679d1fe1d41c892b243fa3f86f8b8e81fc3
Adriana Saty <adri-saty@hotmail.com>
2021-01-18 15:42:58 -0300

    sending reponses with res.write()

--- section3-basics/app.js
+++ section3-basics/app.js
@@ -3,7 +3,15 @@ const http = require('http');
 
 
 const server = http.createServer(((req, res) => {
-    console.log(req)
+    console.log('req',req.url)
+    console.log('method',req.method)
+    console.log('headers',req.headers)
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head>hiii</head>')
+    res.write('</html>')
+    res.end()
 }));
 
-server.listen(3000);
+server.listen(3001);
